c062a31e1fcd7db1ac377f0e033522a99c950564
Vaidehi Sinha <1032201688@mitwpu.edu.in>
2024-01-27 19:39:27 +0530

    Gallery Changes

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -4,6 +4,15 @@ import img2 from "../assets/2.png"
 import img3 from "../assets/3.png"
 import img4 from "../assets/4.png"
 import img5 from "../assets/5.png"
+import img6 from "../assets/6.jpg"
+import img8 from "../assets/8.jpg"
+import img9 from "../assets/9.png"
+import img10 from "../assets/10.png"
+import img11 from "../assets/11.png"
+import img12 from "../assets/12.png"
+import img13 from "../assets/13.png"
+
+
 
 
 
@@ -28,6 +37,29 @@ export const testimonialsData = [
         id: 5,
         image: img5
     },
-    
+    {
+        id: 6,
+        image: img6
+    },
+    {
+        id: 8,
+        image: img8
+    },
+    {
+        id: 9,
+        image: img9
+    },{
+        id: 10,
+        image: img10
+    },{
+        id: 11,
+        image: img11
+    },{
+        id: 12,
+        image: img12
+    },{
+        id: 13,
+        image: img13
+    },
 ]
 
