38b348d4501e3531bc4d094866103757e08e6c80
Vaidehi Sinha <1032201688@mitwpu.edu.in>
2024-01-27 19:06:14 +0530

    Achievement Changes

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -9,33 +9,42 @@ import SmartSeek from "../assets/png/Smart Seek.jpeg"
 import IEEEWISC from "../assets/png/IEEEWISC.png"
 import HackMITWPU from "../assets/png/HackMITWPU.png"
 import MITWPU from "../assets/png/MITWPU.png"
+import BOS from "../assets/png/BestOutgoingStudent.jpg"
+
 
 export const achievementData = {
     bio : "Welcome to a showcase of milestones and triumphs! In this section, I am excited to share with you the achievements that mark significant moments in my professional journey. Each accomplishment reflects my dedication, skills, and the continuous pursuit of excellence. Dive in to discover the milestones that not only define my career but also fuel my passion for continuous growth and innovation.",
     achievements : [
         {
             id : 1,
+            title : 'Best Outgoing Student',
+            details : 'I was awarded the "Best Outgoing Student for the Academic Year 2023-2024" (B. Tech) at MIT World Peace University along with the esteemed "Shri Gold Medal," a distinction established by Prof. Dr. Vishwanath Karad.',
+            date : 'January, 2024',
+            image : BOS
+        },
+        {
+            id : 2,
             title : "IEEE WISC'23 Scholar",
             details : 'I had the honor of being the only recipient for IEEE WISC Scholarship 2023 from India and one amongst the two undergraduates selected from across the world amongst an elite group of 15 distinguished individuals.',
             date : 'July, 2023',
             image : IEEEWISC
         },
         {
-            id : 2,
+            id : 3,
             title : 'Runner Up: Research Poster Presentation Competition',
             details : 'I bagged the Runner Up position for my original research work at WE Local Bengaluru Conference 2023 conducted by Society of Women Engineers.',
             date : 'April, 2023',
             image : Welocal
         },
         {
-            id : 3,
+            id : 4,
             title : 'Winner : MadeInMITWPU at HackMITWPU 2023',
             details : "Team 'AGrowTech' won the MadeInMITWPU track of HackMITWPU 2023 for building and prototyping our winning idea from HackMITWPU Ideathon track 2022, 'GrainLog'.",
             date : 'March, 2023',
             image : HackMITWPU
         },
         {
-            id : 4,
+            id : 5,
             title : 'Scholarship Recipient : Women in Tech Global Conference 2022',
             details : "Received Scholarship to attend the annual Women in Tech Global Conference",
             date : 'May, 2023',
@@ -43,68 +52,69 @@ export const achievementData = {
             
         },
         {
-            id : 5,
+            id : 6,
             title : 'Winner: Blackcoffer Insights 42',
             details : "Ranked first for my article titled : Gender Representation and Diversity in Advertising",
             date : 'July, 2022',
             image : Blackcoffer
         },
         {
-            id : 6,
+            id : 7,
             title : 'WE Local India 2022 Scholarship',
             details : "Received Scholarship to attend the annual WE Loal Conference",
             date : 'April, 2022',
             image : Welocal
         },
         {
-            id : 7,
+            id : 8,
             title : 'Second Position : Smart Seek',
             details : "Ranked Second in the Technical Poster Making Competition for the topic 'Quantum Computing'",
             date : 'March, 2022',
             image : SmartSeek
         },
         {
-            id : 8,
+            id : 9,
             title : 'Finalist : Hack - A – Web',
             details : "Team got selected as one of the top 18 finalists among 250+ teams registered for the final round of hackathon conducted by Entrepreneurship Cell NIT-Bhopal",
             date : 'February, 2022',
             image : Hackaweb
         },
         {
-            id : 9,
+            id : 10,
             title : 'Winner : Bring-O-Pedia',
             details : "I stood first in Dissect: Bring-O-Pedia for my presentation on the topic 'Artificial Emotional Intelligence' conducted by the MIS & Analytics Club, D-Insights of International Management Institute, Kolkata under their event Confero10.0",
             date : 'February, 2022',
             image : Bringopedia
         },
          {
-            id : 10,
+            id : 11,
             title : 'Winner : HackMITWPU Ideathon 2022',
             details : "Our group 'Team AGrow Tech' ranked first in HackMITWPU Innovation Hackathon (Ideathon Track) from School of ECE. HackMITWPU was a prestigious competition which had over 3500 students in over 500 teams.",
             date : 'January, 2022',
             image : HackMITWPU
         },
         {
-            id : 11,
+            id : 12,
             title : 'Winner (Round 1) : IdeaIn10',
             details : "I was announced as the 'Winner' in Round1 of Presentation Competition 'IdeaIn10' conducted by MIT-WPU School of Computer Engineering and Technology in association with IET Pune Local Network",
             date : 'December, 2021',
             image : Ideain10
         },
         {
-            id : 12,
+            id : 13,
             title : 'MIT-WPU Merit Scholarship',
             details : "I was awarded MIT-WPU Merit Scholarship in all years of engineering for consistently being in the top 1% of my batch.",
             date : 'Since December, 2020',
             image: MITWPU,
         },
         {
-            id : 13,
+            id : 14,
             title : 'AIR 30: National Engineering Olympiad 4.0',
             details : "I secured All India Rank 30 in the National Engineering Olympiad 4.0.",
             date : 'July, 2021',
             image: NEO
-        },
+        }
+        
 
     ]
 }
